f0f6c9b055a509532ff88963353d3fc06df6f748
Amanda Steigerwalt <asteig@gmail.com>
2012-06-18 17:29:02 -0700

    Cleaned up test scripts, changed CSS prefix

--- tests/js/ImageAdjusterTests.js
+++ tests/js/ImageAdjusterTests.js
@@ -11,66 +11,67 @@ https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
 
 // Declare dependencies
 /*global fluid, jqUnit, jQuery*/
+/*jslint browser:true */
 
 jQuery(document).ready(function () {
 
-	var imageAdjustmentTests = new jqUnit.testCase("Image Editor Tests");
-	var cssPrefix = '.flc-image-adjustments-';
-	var Adjustments = {}; //empty object for test
+	"use strict";
+
+	var imageAdjustmentTests, cssPrefix, Adjustments, module;
+
+	imageAdjustmentTests = new jqUnit.testCase("Image Editor Tests");
+	cssPrefix = '.flc-image-adjuster-';
+	Adjustments = {}; //empty object for test
 
 	module("Initilization");
 
-	imageAdjustmentTests.test('Setup Tests', function() {
-		jqUnit.isVisible("Main container is visible", cssPrefix+'container');
-		jqUnit.isVisible("Canvas element is visible", cssPrefix+'canvas');
+	imageAdjustmentTests.test('Setup Tests', function () {
+		jqUnit.isVisible("Main container is visible", cssPrefix + 'container');
+		jqUnit.isVisible("Canvas element is visible", cssPrefix + 'canvas');
 	});
 
 	module("Brightness");
 
-	imageAdjustmentTests.test('Brightness Tests', function(){
-		jqUnit.isVisible("Brightness Button is Initially Visible", cssPrefix+"button-brightnesscontrast");
+	imageAdjustmentTests.test('Brightness Tests', function () {
+		jqUnit.isVisible("Brightness Button is Initially Visible", cssPrefix + "button-brightnesscontrast");
 		jqUnit.assertNotUndefined("Brightness value is set in adjustments object", Adjustments.brightness);
 		jqUnit.assertTrue("Adjustments has setBrightness function", Adjustments.hasOwnProperty('setBrightness'));
-		jqUnit.assertTrue("setBrightness accepts values", Adjustments.hasOwnProperty('setBrightness')&&Adjustments.setBrightness(150));
+		jqUnit.assertTrue("setBrightness accepts values", Adjustments.hasOwnProperty('setBrightness') && Adjustments.setBrightness(150));
 		Adjustments.setBrightness(151);
-		jqUnit.assertTrue("Brightness value is within bounds", 
-			Adjustments.hasOwnProperty('brightness')&&Adjustments.brightness>=-150&&Adjustments.brightness<=150);
+		jqUnit.assertTrue("Brightness value is within bounds", Adjustments.hasOwnProperty('brightness') && Adjustments.brightness >= -150 && Adjustments.brightness <= 150);
 	});
 
 	module("Contrast");
 
-	imageAdjustmentTests.test('Contrast Tests', function() {
-		jqUnit.isVisible("Contrast Button is Initially Visible", cssPrefix+"button-brightnesscontrast");
+	imageAdjustmentTests.test('Contrast Tests', function () {
+		jqUnit.isVisible("Contrast Button is Initially Visible", cssPrefix + "button-brightnesscontrast");
 		jqUnit.assertNotUndefined("Contrast value is set in adjustments object", Adjustments.contrast);
 		jqUnit.assertTrue("Adjustments has setContrast function", Adjustments.hasOwnProperty('setContrast'));
-		jqUnit.assertTrue("setContrast accepts values", Adjustments.hasOwnProperty('setContrast')&&Adjustments.setContrast(100));
+		jqUnit.assertTrue("setContrast accepts values", Adjustments.hasOwnProperty('setContrast') && Adjustments.setContrast(100));
 		Adjustments.setContrast(101);
-		jqUnit.assertTrue("Contrast value stays within bounds", 
-			Adjustments.hasOwnProperty('contrast')&&Adjustments.contrast>=-50&&Adjustments.contrast<=100);
+		jqUnit.assertTrue("Contrast value stays within bounds", Adjustments.hasOwnProperty('contrast') && Adjustments.contrast >= -50 && Adjustments.contrast <= 100);
 	});
 
 	module("Rotation");
 
-	imageAdjustmentTests.test('Rotation Tests', function() {
-		jqUnit.isVisible("Rotation Button is Initially Visible", cssPrefix+"button-rotate");
+	imageAdjustmentTests.test('Rotation Tests', function () {
+		jqUnit.isVisible("Rotation Button is Initially Visible", cssPrefix + "button-rotate");
 		jqUnit.assertNotUndefined("Rotation value is set in adjustments object", Adjustments.rotation);
 		jqUnit.assertTrue("Adjustments has setRotation function", Adjustments.hasOwnProperty('setRotation'));
-		jqUnit.assertTrue("setRotation accepts values", Adjustments.hasOwnProperty('setRotation')&&Adjustments.setRotation(360));
+		jqUnit.assertTrue("setRotation accepts values", Adjustments.hasOwnProperty('setRotation') && Adjustments.setRotation(360));
 		Adjustments.setRotation(361);
-		jqUnit.assertTrue("Rotation value stays within bounds", 
-			Adjustments.hasOwnProperty('rotation')&&Adjustments.rotation>=-360&&Adjustments.contrast<=360);
+		jqUnit.assertTrue("Rotation value stays within bounds", Adjustments.hasOwnProperty('rotation') && Adjustments.rotation >= -360 && Adjustments.contrast <= 360);
 	});
 
 	module("Threshold");
 
 	imageAdjustmentTests.test('Threshold Tests', function () {
-		jqUnit.isVisible("Threshold Button is Initially Visible", cssPrefix+("button-threshold"));
+		jqUnit.isVisible("Threshold Button is Initially Visible", cssPrefix + ("button-threshold"));
 		jqUnit.assertNotUndefined("Threshold value is set in adjustments object", Adjustments.threshold);
 		jqUnit.assertTrue("Adjustments has setThreshold function", Adjustments.hasOwnProperty('setThreshold'));
-		jqUnit.assertTrue("setThreshold accepts values", Adjustments.hasOwnProperty('setThreshold')&&Adjustments.setThreshold(128));
-		Assessments.setThreshold(129);
-		jqUnit.assertTrue("Threshold value stays within bounds", 
-			Adjustments.hasOwnProperty('threshold')&&Adjustments.threshold>=0&&Adjustments.threshold<=128);
+		jqUnit.assertTrue("setThreshold accepts values", Adjustments.hasOwnProperty('setThreshold') && Adjustments.setThreshold(128));
+		Adjustments.setThreshold(129);
+		jqUnit.assertTrue("Threshold value stays within bounds", Adjustments.hasOwnProperty('threshold') && Adjustments.threshold >= 0 && Adjustments.threshold <= 128);
 	});
 
 });
