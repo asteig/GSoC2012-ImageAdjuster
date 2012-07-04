0578f2490024d171de11fff57918a3d48ccf1b03
Amanda Steigerwalt <asteig@gmail.com>
2012-07-04 01:53:06 -0700

    Modified initialization tests

--- tests/js/ImageAdjusterTests.js
+++ tests/js/ImageAdjusterTests.js
@@ -23,11 +23,9 @@ jQuery(document).ready(function () {
 	module("Initilization");
 
 	imageAdjustmentTests.test('Setup Tests', function () {
-		jqUnit.assertTrue('Fluid imageAdjuster object exists', fluid.imageAdjuster);
-		jqUnit.assertNotUndefined('Default selectors defined', fluid.defaults);
-		jqUnit.assertNotUndefined("imageAdjuster has adjustments object", fluid.imageAdjuster.adjustments);
-		jqUnit.assertNotUndefined("Fluid has default values set", fluid.defaults.selectors);
-		
+		jqUnit.assertTrue('Fluid imageAdjuster object exists', editor.imageAdjuster);
+		jqUnit.assertNotUndefined("imageAdjuster has adjustments object", editor.imageAdjuster.adjustments);	
+		jqUnit.assertNotUndefined('Default selectors defined', fluid.defaults.selectors);
 		//throws an error when fluid.defaults.selectors is not defined :(
 		jQuery.each(fluid.defaults.selectors, function(k, v) {
 			jqUnit.assertTrue('Default selector '+v+' appears on page', $(v));
@@ -38,41 +36,41 @@ jQuery(document).ready(function () {
 	module("Brightness");
 	
 	imageAdjustmentTests.test('Brightness Tests',	function () {	
-		jqUnit.assertNotUndefined("Brightness value is set in adjustments object", fluid.imageAdjuster.adjustments.brightness);
-		jqUnit.assertTrue("Adjustments has setBrightness method", fluid.imageAdjuster.hasOwnProperty('setBrightness'));
-		jqUnit.assertTrue("setBrightness accepts values", fluid.imageAdjuster.hasOwnProperty('setBrightness') && fluid.imageAdjuster.setBrightness(50) === 50);
-		jqUnit.assertTrue("setBrightness adjusts for values below lower bound", fluid.imageAdjuster.hasOwnProperty('setBrightness') && fluid.imageAdjuster.setBrightness(-151) === -150);
-		jqUnit.assertTrue("setBrightness adjusts for values above lower bound", fluid.imageAdjuster.hasOwnProperty('setBrightness') && fluid.imageAdjuster.setBrightness(151) === 150);
+		jqUnit.assertNotUndefined("Brightness value is set in adjustments object", editor.imageAdjuster.adjustments.brightness);
+		jqUnit.assertTrue("Adjustments has setBrightness method", editor.imageAdjuster.hasOwnProperty('setBrightness'));
+		jqUnit.assertTrue("setBrightness accepts values", editor.imageAdjuster.hasOwnProperty('setBrightness') && editor.imageAdjuster.setBrightness(50) === 50);
+		jqUnit.assertTrue("setBrightness adjusts for values below lower bound", editor.imageAdjuster.hasOwnProperty('setBrightness') && editor.imageAdjuster.setBrightness(-151) === -150);
+		jqUnit.assertTrue("setBrightness adjusts for values above lower bound", editor.imageAdjuster.hasOwnProperty('setBrightness') && editor.imageAdjuster.setBrightness(151) === 150);
 	});
 
 	module("Contrast");
 
 	imageAdjustmentTests.test('Contrast Tests', function () {
-		jqUnit.assertNotUndefined("Contrast value is set in adjustments object", fluid.imageAdjuster.adjustments.contrast);
-		jqUnit.assertTrue("Adjustments has setContrast method", fluid.imageAdjuster.hasOwnProperty('setContrast'));
-		jqUnit.assertTrue("setContrast accepts values", fluid.imageAdjuster.hasOwnProperty('setContrast') && fluid.imageAdjuster.setContrast(50) === 50);
-		jqUnit.assertTrue("setContrast adjusts for values below lower bound", fluid.imageAdjuster.hasOwnProperty('setContrast') && fluid.imageAdjuster.setContrast(-151) === -150);
-		jqUnit.assertTrue("setContrast adjusts for values above lower bound", fluid.imageAdjuster.hasOwnProperty('setContrast') && fluid.imageAdjuster.setContrast(151) === 150);
+		jqUnit.assertNotUndefined("Contrast value is set in adjustments object", editor.imageAdjuster.adjustments.contrast);
+		jqUnit.assertTrue("Adjustments has setContrast method", editor.imageAdjuster.hasOwnProperty('setContrast'));
+		jqUnit.assertTrue("setContrast accepts values", editor.imageAdjuster.hasOwnProperty('setContrast') && editor.imageAdjuster.setContrast(50) === 50);
+		jqUnit.assertTrue("setContrast adjusts for values below lower bound", editor.imageAdjuster.hasOwnProperty('setContrast') && editor.imageAdjuster.setContrast(-151) === -150);
+		jqUnit.assertTrue("setContrast adjusts for values above lower bound", editor.imageAdjuster.hasOwnProperty('setContrast') && editor.imageAdjuster.setContrast(151) === 150);
 	});
 		
 
 	module("Rotation");
 
 	imageAdjustmentTests.test('Rotation Tests', function () {
-		jqUnit.assertNotUndefined("Rotation value is set in adjustments object", fluid.imageAdjuster.adjustments.rotation);
-		jqUnit.assertTrue("ImageAdjuster has setRotation function", fluid.imageAdjuster.hasOwnProperty('setRotate'));
-		jqUnit.assertTrue("setRotate accepts values", fluid.imageAdjuster.hasOwnProperty('setRotate') && fluid.imageAdjuster.setRotate(60) === 60);
-		jqUnit.assertTrue("setRotate adjusts for values below lower bound", fluid.imageAdjuster.hasOwnProperty('setRotate') && parseInt(fluid.imageAdjuster.setRotate(-361)) === parseInt(-360));
-		jqUnit.assertTrue("setRotate adjusts for value above upper bound", fluid.imageAdjuster.hasOwnProperty('setRotate') && fluid.imageAdjuster.setRotate(361) === (360));
+		jqUnit.assertNotUndefined("Rotation value is set in adjustments object", editor.imageAdjuster.adjustments.rotation);
+		jqUnit.assertTrue("ImageAdjuster has setRotation function", editor.imageAdjuster.hasOwnProperty('setRotate'));
+		jqUnit.assertTrue("setRotate accepts values", editor.imageAdjuster.hasOwnProperty('setRotate') && editor.imageAdjuster.setRotate(60) === 60);
+		jqUnit.assertTrue("setRotate adjusts for values below lower bound", editor.imageAdjuster.hasOwnProperty('setRotate') && parseInt(editor.imageAdjuster.setRotate(-361)) === parseInt(-360));
+		jqUnit.assertTrue("setRotate adjusts for value above upper bound", editor.imageAdjuster.hasOwnProperty('setRotate') && editor.imageAdjuster.setRotate(361) === (360));
 	});
 
 	module("Threshold");
 
 	imageAdjustmentTests.test('Threshold Tests', function () {
-		jqUnit.assertNotUndefined("Threshold value is set in adjustments object", fluid.imageAdjuster.adjustments.threshold);
-		jqUnit.assertTrue("Adjustments has setThreshold function", fluid.imageAdjuster.hasOwnProperty('setThreshold'));
-		jqUnit.assertTrue("setThreshold accepts values", fluid.imageAdjuster.hasOwnProperty('setThreshold') && fluid.imageAdjuster.setThreshold(128));
-		jqUnit.assertTrue("setThreshold adjusts for values below lower  bound", fluid.imageAdjuster.hasOwnProperty('setThreshold') && fluid.imageAdjuster.setThreshold(-1) === 0);
-		jqUnit.assertTrue("setThreshold adjusts for values above higher bound", fluid.imageAdjuster.hasOwnProperty('setThreshold') && fluid.imageAdjuster.setThreshold(129) === 128);	
+		jqUnit.assertNotUndefined("Threshold value is set in adjustments object", editor.imageAdjuster.adjustments.threshold);
+		jqUnit.assertTrue("Adjustments has setThreshold function", editor.imageAdjuster.hasOwnProperty('setThreshold'));
+		jqUnit.assertTrue("setThreshold accepts values", editor.imageAdjuster.hasOwnProperty('setThreshold') && editor.imageAdjuster.setThreshold(128));
+		jqUnit.assertTrue("setThreshold adjusts for values below lower  bound", editor.imageAdjuster.hasOwnProperty('setThreshold') && editor.imageAdjuster.setThreshold(-1) === 0);
+		jqUnit.assertTrue("setThreshold adjusts for values above higher bound", editor.imageAdjuster.hasOwnProperty('setThreshold') && editor.imageAdjuster.setThreshold(129) === 128);	
 	});
 });
